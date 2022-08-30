d35b745ed0ee0311ba3f53552144a56ae7500440
David Andrs <andrsd@gmail.com>
2022-08-30 15:58:22 -0600

    Adding python tests

---
+++ python/tests/test_nitrogen.py
@@ -0,0 +1,14 @@
+import pyfprops as fp
+import pytest
+
+def test_n2_valid():
+    n2 = fp.Nitrogen()
+    state = n2.p_T(1e6, 280)
+    assert state.p == 1e6
+    assert state.T == 280
+    assert state.rho == 12.074993445601923
+
+def test_n2_invalid():
+    with pytest.raises(RuntimeError):
+        n2 = fp.Nitrogen()
+        state = n2.p_T(0.1e6, 60)
