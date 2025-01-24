df8668e38d5d8f569f9e591450f61cd3f148c54e
David Andrs <andrsd@gmail.com>
2025-01-24 08:37:40 -0700

    Python bindings use the actual C++ class inheritance

--- python/src/fprops.cpp
+++ python/src/fprops.cpp
@@ -4,9 +4,10 @@
 #include <pybind11/pybind11.h>
 
 #include "fprops/single_phase_fluid_properties.h"
+#include "fprops/ideal_gas.h"
+#include "fprops/helmholtz.h"
 #include "fprops/air.h"
 #include "fprops/ammonia.h"
-#include "fprops/ideal_gas.h"
 #include "fprops/nitrogen.h"
 #include "fprops/helium.h"
 #include "fprops/carbon_dioxide.h"
@@ -23,9 +24,14 @@ PYBIND11_MODULE(fprops, m)
     m.doc() = "pybind11 plugin for fprops";
     py::setattr(m, "version", py::str(FPROPS_VERSION));
 
-    py::class_<SinglePhaseFluidProperties> spfp(m, "SinglePhaseFluidProperties");
+    py::class_<SinglePhaseFluidProperties>(m, "SinglePhaseFluidProperties")
+        .def("rho_T", &SinglePhaseFluidProperties::rho_T)
+        .def("rho_p", &SinglePhaseFluidProperties::rho_p)
+        .def("p_T", &SinglePhaseFluidProperties::p_T)
+        .def("v_u", &SinglePhaseFluidProperties::v_u)
+        .def("h_s", &SinglePhaseFluidProperties::h_s);
 
-    py::class_<State>(spfp, "State")
+    py::class_<State>(m, "State")
         .def(py::init<>())
         .def_readwrite("u", &State::u)
         .def_readwrite("v", &State::v)
@@ -42,64 +48,31 @@ PYBIND11_MODULE(fprops, m)
         .def_readwrite("w", &State::w)
         .def("__repr__", &State::to_string);
 
-    py::class_<Air>(m, "Air")
-        .def(py::init())
-        .def("rho_T", &Air::rho_T)
-        .def("rho_p", &Air::rho_p)
-        .def("p_T", &Air::p_T)
-        .def("v_u", &Air::v_u);
+    py::class_<Helmholtz, SinglePhaseFluidProperties>(m, "Helmholtz");
 
-    py::class_<Ammonia>(m, "Ammonia")
-        .def(py::init())
-        .def("rho_T", &Ammonia::rho_T)
-        .def("rho_p", &Ammonia::rho_p)
-        .def("p_T", &Ammonia::p_T)
-        .def("v_u", &Ammonia::v_u);
+    py::class_<Air, Helmholtz>(m, "Air").def(py::init());
 
-    py::class_<IdealGas>(m, "IdealGas")
+    py::class_<Ammonia, Helmholtz>(m, "Ammonia").def(py::init());
+
+    py::class_<IdealGas, SinglePhaseFluidProperties>(m, "IdealGas")
         .def(py::init<double, double>())
         .def("gamma", &IdealGas::gamma)
+        .def("molar_mass", &IdealGas::molar_mass)
         .def("specific_gas_constant", &IdealGas::R_specific)
+        .def("cp", &IdealGas::cp)
+        .def("cv", &IdealGas::cv)
+        .def("mu", &IdealGas::mu)
+        .def("k", &IdealGas::k)
         .def("set_mu", &IdealGas::set_mu)
-        .def("set_k", &IdealGas::set_k)
-        .def("rho_T", &IdealGas::rho_T)
-        .def("rho_p", &IdealGas::rho_p)
-        .def("p_T", &IdealGas::p_T)
-        .def("v_u", &IdealGas::v_u)
-        .def("h_s", &IdealGas::h_s);
+        .def("set_k", &IdealGas::set_k);
 
-    py::class_<Nitrogen>(m, "Nitrogen")
-        .def(py::init())
-        .def("rho_T", &Nitrogen::rho_T)
-        .def("rho_p", &Nitrogen::rho_p)
-        .def("p_T", &Nitrogen::p_T)
-        .def("v_u", &Nitrogen::v_u);
+    py::class_<Nitrogen, Helmholtz>(m, "Nitrogen").def(py::init());
 
-    py::class_<Helium>(m, "Helium")
-        .def(py::init())
-        .def("rho_T", &Helium::rho_T)
-        .def("rho_p", &Helium::rho_p)
-        .def("p_T", &Helium::p_T)
-        .def("v_u", &Helium::v_u);
+    py::class_<Helium, Helmholtz>(m, "Helium").def(py::init());
 
-    py::class_<CarbonDioxide>(m, "CarbonDioxide")
-        .def(py::init())
-        .def("rho_T", &CarbonDioxide::rho_T)
-        .def("rho_p", &CarbonDioxide::rho_p)
-        .def("p_T", &CarbonDioxide::p_T)
-        .def("v_u", &CarbonDioxide::v_u);
+    py::class_<CarbonDioxide, Helmholtz>(m, "CarbonDioxide").def(py::init());
 
-    py::class_<Oxygen>(m, "Oxygen")
-        .def(py::init())
-        .def("rho_T", &Oxygen::rho_T)
-        .def("rho_p", &Oxygen::rho_p)
-        .def("p_T", &Oxygen::p_T)
-        .def("v_u", &Oxygen::v_u);
+    py::class_<Oxygen, Helmholtz>(m, "Oxygen").def(py::init());
 
-    py::class_<Methane>(m, "Methane")
-        .def(py::init())
-        .def("rho_T", &Methane::rho_T)
-        .def("rho_p", &Methane::rho_p)
-        .def("p_T", &Methane::p_T)
-        .def("v_u", &Methane::v_u);
+    py::class_<Methane, Helmholtz>(m, "Methane").def(py::init());
 }
